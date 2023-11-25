eb76ace58ecf3968d84790fde0a0e1e93249581f
lujana <lujanabajra@gmail.com>
2023-11-25 11:56:48 -0500

    display login on the homepage

--- app_ofs/urls.py
+++ app_ofs/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login', views.login_view, name='login'),
+    path('login/', views.login_view, name='login'),
     path('register/', views.register, name = 'register'),
     path('dashboard', views.user_dashboard, name = 'dashboard'),
     path('logout/', views.logout_view, name='logout'),
