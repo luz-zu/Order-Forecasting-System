c3b524b7e1c177705270d1f91ae3d737fcc75b79
Lujana Bajracharya <lujanabajra@gmail.com>
2023-12-20 10:27:15 -0500

    user role attributes

--- app_ofs/models.py
+++ app_ofs/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     dob = models.DateField(null=True, blank=True)
     phone_number = models.CharField(max_length=15, blank=True)
+    userrole = models.CharField(max_length=15, blank=True)
 
 class category(models.Model):
     category_id = models.CharField(max_length=100)
